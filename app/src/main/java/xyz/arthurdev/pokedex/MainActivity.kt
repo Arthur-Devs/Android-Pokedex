3c26411dc2c9a35225562adc47fd0a43d578d85c
ArthurB.Dev <>
2023-02-16 22:14:19 +0100

    setup navigation with FragmentContainerView

--- app/src/main/java/xyz/arthurdev/pokedex/MainActivity.kt
+++ app/src/main/java/xyz/arthurdev/pokedex/MainActivity.kt
@@ -2,10 +2,30 @@ package xyz.arthurdev.pokedex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        // Because we're creating the NavHostFragment using FragmentContainerView, we must
+        // retrieve the NavController directly from the NavHostFragment instead
+        print(R.id.nav_host_fragment)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        print(navHostFragment)
+        val navController = navHostFragment.navController
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp()
     }
 }
